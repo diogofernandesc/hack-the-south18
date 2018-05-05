1c6eeb0d4ee76dac9f1be4445ed98778a1394fd6
James Curran <lggy@hotmail.co.uk>
2018-05-05 17:18:24 +0100

    Homepage
    Kinda

--- populacious/polls/urls.py
+++ populacious/polls/urls.py
@@ -7,13 +7,5 @@ urlpatterns = [
 	# ex: /polls/
     url(r'^$', views.IndexView.as_view(), name='index'),
 	# ex: /polls/5/
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-	# ex: /polls/submit/done
-	url(r'^submit/done/$', views.done, name='done'),
-	# ex: /polls/submit/
-	url(r'^submit/$', views.submit, name='submit'),
+    url(r'^(?P<pk>[a-z]+)/$', views.DetailView.as_view(), name='detail')
 ]
